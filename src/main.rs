a2a3c8cfa8133de4d11aacca2223f5c6eeadf458
jerlendds <jerlendds@proton.me>
2025-05-26 11:17:17 -0600

    add threads

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use pyo3::{prelude::*};
-use pyo3::types::{IntoPyDict};
-use pyo3::ffi::c_str;
+use pyo3::ffi::{PyObject, c_str};
+use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
+use std::thread;
+use std::time::Duration;
 
-fn main() -> PyResult<()> {
-    Python::with_gil(|py| {
-        let locals = [("selenium", py.import("selenium")?)].into_py_dict(py)?;
-        let code = c_str!(r#"
+fn main() {
+    let t1 = thread::spawn(move || {
+        for _ in 1..5 {
+            Python::with_gil(|py| {
+                let locals = [("selenium", py.import("selenium").unwrap())].into_py_dict(py);
+                let code = c_str!(r#"
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -14,14 +18,30 @@ driver.get('http://www.python.org')
 assert 'Python' in driver.title
 elm = driver.find_element(By.NAME, 'q')
 print(elm)
-    "#);
-        Python::run(py, code, None, Some(&locals))?;
+                "#);
+                Python::run(py, code, None, Some(&locals.unwrap())).unwrap();
+            });
+        }
+    });
+    let t2 = thread::spawn(move || {
+        for _ in 1..5 {
+            Python::with_gil(|py| {
+                let locals = [("httpx", py.import("httpx").unwrap())]
+                    .into_py_dict(py)
+                    .unwrap();
+                let code = c_str!("httpx.get('https://google.com')");
+                let httpx_resp = py.eval(code, None, Some(&locals)).unwrap();
 
-        let locals = [("httpx", py.import("httpx")?)].into_py_dict(py)?;
-        let code = c_str!("httpx.get('https://google.com')");
-        let httpx_resp: PyObject = py.eval(code, None, Some(&locals))?.extract()?;
+                println!("httpx test: {}", httpx_resp);
+            });
+        }
+    });
 
-        println!("httpx test: {}", httpx_resp);
-        Ok(())
-    })
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
 }
