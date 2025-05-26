a887eca2db25c7ef145709be4af57ded76349591
jerlendds <jerlendds@proton.me>
2025-05-26 10:15:43 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use pyo3::{prelude::*, py_run};
+use pyo3::types::{IntoPyDict, PyFunction};
+use pyo3::ffi::c_str;
+#[pyclass]
+struct UserData {
+    id: u32,
+    name: String,
+}
+
+#[pymethods]
+impl UserData {
+    fn as_tuple(&self) -> (u32, String) {
+        (self.id, self.name.clone())
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("User {}(id: {})", self.name, self.id))
+    }
+}
+fn main() -> PyResult<()> {
+    Python::with_gil(|py| {
+        let locals = [("selenium", py.import("selenium")?)].into_py_dict(py)?;
+        let code = c_str!(r#"
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+driver = webdriver.Firefox()
+driver.get('http://www.python.org')
+assert 'Python' in driver.title
+elm = driver.find_element(By.NAME, 'q')
+print(elm)
+    "#);
+        Python::run(py, code, None, Some(&locals))?;
+
+        let locals = [("httpx", py.import("httpx")?)].into_py_dict(py)?;
+        let code = c_str!("httpx.get('https://google.com')");
+        let httpx_resp: PyObject = py.eval(code, None, Some(&locals))?.extract()?;
+
+        println!("httpx test: {}", httpx_resp);
+        Ok(())
+    })
+}
